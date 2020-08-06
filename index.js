99baf936340c1e995bf5ec504f5b5fee98af672d
Jonathan <jonathanthornton1997@gmail.com>
2020-08-06 11:04:57 -0700

    finished stretch

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-require("dotenv").config()
+require("dotenv").config();
 
-const express = require('express')
-const cors = require('cors')
-const server = express()
-const port = process.env.PORT || 5000
-const postsRouter = require('./routers/posts-router')
-server.use(express.json())
-server.use(cors())
+const express = require("express");
+const cors = require("cors");
+const server = express();
+const port = process.env.PORT || 5000;
+const postsRouter = require("./routers/posts-router");
+server.use(express.json());
+server.use(cors());
 
-server.get('/', (req, res) => {
-    const message = process.env.MESSAGE 
-    res.send(`<h1>Server is running</h1>`)
-})
+server.get("/", (req, res) => {
+  const message = process.env.MESSAGE;
+  res.send(`<h1>Server is running {message} </h1>`);
+});
 
-
-server.use('/api/posts', postsRouter)
-
-
+server.use("/api/posts", postsRouter);
 
 server.listen(port, () => {
-    console.log('\n*** Server Running on http://localhost:4000 ***\n')
-})
+  console.log("\n*** Server Running on http://localhost:4000 ***\n");
+});
